50157680206faa31c1afdcd6177207509c8d6903
Tomasz Kielar <git@k1eu.dev>
2025-10-08 09:36:53 +0200

    feat: add necessary tools

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import { createOpenAI } from "@ai-sdk/openai";
+import { generateText, type ModelMessage } from "ai";
+import { SQL } from "bun";
+
+const pg = new SQL(process.env.POSTGRES_URL!);
+
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": async () => {
+      const response = await generateText({
+        model: openai.responses("gpt-4o"),
+        messages: [
+          {
+            role: "user",
+            content: "Write a haiku about Bun.js",
+          },
+        ],
+      });
+
+      return new Response(response.text);
+    },
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
